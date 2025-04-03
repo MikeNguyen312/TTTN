093123792e6e2a7467de7bf91e0973bbaef7e56a
loc <loc@gmail.com>
2025-04-03 11:50:57 +0700

    first step

---
+++ WebAPI_ForTTTN/WebAPI_ForTTTN/Models/DonHang.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI_ForTTTN.Models
+{
+    public partial class DonHang
+    {
+        public DonHang()
+        {
+            ChiTietDonHangs = new HashSet<ChiTietDonHang>();
+        }
+
+        public string IdDonHang { get; set; } = null!;
+        public string? IdKhachHang { get; set; }
+        public DateTime? NgayDatHang { get; set; }
+        public decimal? TongTien { get; set; }
+
+        public virtual KhachHang? IdKhachHangNavigation { get; set; }
+        public virtual ICollection<ChiTietDonHang> ChiTietDonHangs { get; set; }
+    }
+}
