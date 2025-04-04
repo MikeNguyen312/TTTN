292612e2e764c6318d866a2b78f8ca00532a2871
loc <loc@gmail.com>
2025-04-04 18:22:23 +0700

    upload design UI

--- UI_Angular/AngularApp/src/app/pages/donhang/donhang.component.ts
+++ UI_Angular/AngularApp/src/app/pages/donhang/donhang.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DonhangService } from '../../services/donhang.service';
 
 @Component({
   selector: 'app-donhang',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './donhang.component.html',
   styleUrl: './donhang.component.css'
 })
 export class DonhangComponent {
+donHangs: any[] = [];
 
+  // Inject the service in the constructor
+  constructor(private donhangService: DonhangService) {}
+
+  ngOnInit(): void {
+    // Load products when the component initializes
+    this.loadDonHang();
+  }
+
+  loadDonHang(): void {
+    this.donhangService.getDonHangs().subscribe({
+      next: (data) => {
+        console.log('Fetched data:', data); 
+        this.donHangs = Array.isArray(data) ? data : []; // Ensure data is an array
+      },
+      error: (err) => {
+        console.error('Error fetching DonHang:', err);
+      }
+    });
+  }
 }
