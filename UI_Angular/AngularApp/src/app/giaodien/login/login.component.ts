26d518b84f4e0b95e4f4fd76575dadac6308c85c
MikeNguyen312 <nguyenminhhuy0312@gmail.com>
2025-04-24 18:44:01 +0700

    ss

--- UI_Angular/AngularApp/src/app/giaodien/login/login.component.ts
+++ UI_Angular/AngularApp/src/app/giaodien/login/login.component.ts
@@ -15,39 +15,42 @@ export class LoginComponent {
   email = '';
   password = '';
   errorMessage = '';
-
+  rememberMe = false;
   constructor(private khachHangservice: KhachhangService, private router: Router) {}
 
-  onLogin() {
-    if (this.email === 'admin' && this.password === '1') {
-      localStorage.setItem('username', 'Admin');
-      localStorage.setItem('isAdmin', 'true');
-      this.router.navigate(['/admin/dashboard']);
-      return;
-    }
-  
-    this.khachHangservice.login(this.email, this.password).subscribe({
-      next: (res) => {
-        console.log('Login response:', res);  
-        // Kiểm tra trạng thái trước khi cho đăng nhập
-        if (res.trangthai !== 'Đã xác nhận') {
-          this.errorMessage = 'Tài khoản chưa xác nhận';
-          setTimeout(() => {
-            this.errorMessage = '';
-          }, 1000);
-          return;
-        }
-  
-        // Nếu đã xác nhận thì tiếp tục đăng nhập
-        localStorage.setItem('username', res.hoTen);
-        localStorage.setItem('isAdmin', 'false');
-        this.router.navigate([`/trang-chu/${res.iD_KhachHang}`]);
-      },
-      error: (err) => {
-        console.error('Login failed:', err);
-        this.errorMessage = err.error?.message || 'Đăng nhập thất bại!';
-      }
-    });
+onLogin() {
+  if (this.rememberMe) {
+    localStorage.setItem('keepLoggedIn', 'true');
   }
+  if (this.email === 'admin' && this.password === '1') {
+    localStorage.setItem('username', 'Admin');
+    localStorage.setItem('isAdmin', 'true');
+    localStorage.setItem('userId', 'admin'); 
+    this.router.navigate(['/admin/dashboard']);
+    return;
+  }
+
+  this.khachHangservice.login(this.email, this.password).subscribe({
+    next: (res) => {
+      console.log('Login response:', res);  
+      if (res.trangthai !== 'Đã xác nhận') {
+        this.errorMessage = 'Tài khoản chưa xác nhận';
+        setTimeout(() => {
+          this.errorMessage = '';
+        }, 1000);
+        return;
+      }
+
+      localStorage.setItem('username', res.hoTen);
+      localStorage.setItem('isAdmin', 'false');
+      localStorage.setItem('userId', res.iD_KhachHang.toString()); // Add this
+      this.router.navigate([`/trang-chu/${res.iD_KhachHang}`]);
+    },
+    error: (err) => {
+      console.error('Login failed:', err);
+      this.errorMessage = err.error?.message || 'Đăng nhập thất bại!';
+    }
+  });
+}
   
 }
